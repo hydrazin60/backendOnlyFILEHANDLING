f68bedb7958ae781bd8543a4f9b0f88aa4761349
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-12 11:35:38 +0545

    user.model create

---
+++ index.js
@@ -0,0 +1,32 @@
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = 4000;
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+app.use(cors(corsOptions));
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/filehandling")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      minlength: [3, "Username must be at least 3 characters long"],
+      maxlength: [20, "Username cannot exceed 20 characters"],
+      match: [
+        /^[a-zA-Z0-9-@]+$/,
+        "Username can only contain letters and numbers and @",
+      ],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    coverPic: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "hello there i am using instaclone",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
